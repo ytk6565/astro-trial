3e82aaecb6a03654d2c1e0ec289d52e58a2303eb
ytk6565 <ytk6565@gmail.com>
2024-01-14 23:39:26 +0900

    fisrt commit

---
+++ src/components/vue/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Vue Counter: {{ count }}</h1>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref<number>(0);
+
+const increment = () => {
+  count.value++;
+};
+
+const decrement = () => {
+  count.value--;
+};
+</script>
